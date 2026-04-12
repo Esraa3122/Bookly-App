0e5895bb067c1a600ea5739a34f8fd517ce0a014
Esraa El-sheikh <esraa3122@outlook.com>
2026-04-12 19:02:43 +0200

    add featured List View Item

---
+++ lib/features/home/presentation/views/widgets/featued_list_view_item.dart
@@ -0,0 +1,21 @@
+import 'package:bookly/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class FeatuedListViewItem extends StatelessWidget {
+  const FeatuedListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2.7/4,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          image: const DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage(AssetsData.testImage)),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,5 @@
-import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:bookly/features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:bookly/features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -8,12 +7,7 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const [
-         CustomAppBar(),
-         CustomListViewItem(),
-      ],
-    );
+    return Column(children: const [CustomAppBar(), FeatureBooksListView()]);
   }
 }
 
