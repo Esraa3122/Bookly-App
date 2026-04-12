d20b4b43a4a5d3193d398173ca7171346cdcae3d
Esraa El-sheikh <esraa3122@outlook.com>
2026-04-13 01:48:04 +0200

    create books action

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/bookly_rate.dart';
+import 'package:bookly/features/home/presentation/views/widgets/books_action.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -14,21 +15,34 @@ class BookDetailsViewBody extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
-        const CustomBookDetailsAppBar(),
-         Padding(
-           padding: EdgeInsets.symmetric(horizontal: width * 0.2),
-           child: const CustomBookImage(),
-         ),
+          const CustomBookDetailsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+            child: const CustomBookImage(),
+          ),
           const SizedBox(height: 43),
-         Text('The Jungle Book', style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold)),
+          Text(
+            'The Jungle Book',
+            style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+          ),
           const SizedBox(height: 6),
-         Opacity(
-          opacity: .7,
-          child: Text('The Jungle Book', style: Styles.textStyle18.copyWith(fontStyle: FontStyle.italic, fontWeight: FontWeight.w500))),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              'The Jungle Book',
+              style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
           const SizedBox(height: 20),
           const BookRating(mainAxisAlignment: MainAxisAlignment.center),
+          const SizedBox(height: 37),
+          const BooksAction(),
         ],
       ),
     );
   }
 }
+
