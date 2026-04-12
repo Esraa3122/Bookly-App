aa94669633bbcfe941dec0dc985531b6efc696c8
Esraa El-sheikh <esraa3122@outlook.com>
2026-04-13 01:18:50 +0200

    add title & subtitle to details

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
+import 'package:bookly/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -6,12 +8,22 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
-        children: const [
-         CustomBookDetailsAppBar(),
-
+        children: [
+        const CustomBookDetailsAppBar(),
+         Padding(
+           padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+           child: const CustomBookImage(),
+         ),
+          const SizedBox(height: 43),
+         Text('The Jungle Book', style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold)),
+          const SizedBox(height: 6),
+         Opacity(
+          opacity: .7,
+          child: Text('The Jungle Book', style: Styles.textStyle18.copyWith(fontStyle: FontStyle.italic, fontWeight: FontWeight.w500))),
         ],
       ),
     );
