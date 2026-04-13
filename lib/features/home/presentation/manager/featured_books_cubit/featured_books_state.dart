7e9e2c219b559e6149bf67fe3c6fcbb63d2ba266
Esraa El-sheikh <esraa3122@outlook.com>
2026-04-14 00:10:55 +0200

    create featured books states

---
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
@@ -0,0 +1,21 @@
+part of 'featured_books_cubit.dart';
+
+@immutable
+abstract class FeaturedBooksState {}
+
+class FeaturedBooksInitial extends FeaturedBooksState {}
+
+class FeaturedBooksLoding extends FeaturedBooksState {}
+
+class FeaturedBooksFailure extends FeaturedBooksState {
+  final String errMessage;
+
+  FeaturedBooksFailure(this.errMessage);
+}
+
+class FeaturedBooksSuccess extends FeaturedBooksState {
+  final List<BookEntity> books;
+
+  FeaturedBooksSuccess(this.books);
+
+}
