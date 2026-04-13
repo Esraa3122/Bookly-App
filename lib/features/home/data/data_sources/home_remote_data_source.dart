5d21317c25959732da36aeb1aabe866fae333916
Esraa El-sheikh <esraa3122@outlook.com>
2026-04-13 17:35:36 +0200

    cash featured books

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,4 +1,6 @@
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/api_service.dart';
+import 'package:bookly/core/utils/functions/save_books.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
 
@@ -7,30 +9,33 @@ abstract class HomeRemoteDataSource {
   Future<List<BookEntity>> fetchNewestBooks();
 }
 
-class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
   final ApiService apiService;
   HomeRemoteDataSourceImpl(this.apiService);
   @override
-  Future<List<BookEntity>> fetchFeaturedBooks() async{
-    var data =  await apiService.get(endPoint: 'volumes?filter=free-ebooks&q=programming');
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
+    var data = await apiService.get(
+      endPoint: 'volumes?filter=free-ebooks&q=programming',
+    );
     List<BookEntity> books = getBooksList(data);
+    saveBooksData(books, kFeaturedBox);
     return books;
   }
 
   @override
-   Future<List<BookEntity>> fetchNewestBooks() async{
-    var data =  await apiService.get(endPoint: 'volumes?filter=free-ebooks&Sorting=newest&q=programming');
+  Future<List<BookEntity>> fetchNewestBooks() async {
+    var data = await apiService.get(
+      endPoint: 'volumes?filter=free-ebooks&Sorting=newest&q=programming',
+    );
     List<BookEntity> books = getBooksList(data);
     return books;
   }
 
-    List<BookEntity> getBooksList(Map<String, dynamic> data) {
+  List<BookEntity> getBooksList(Map<String, dynamic> data) {
     List<BookEntity> books = [];
     for (var bookMap in data['items']) {
       books.add(BookModel.fromJson(bookMap));
     }
     return books;
   }
-
-} 
-  
+}
