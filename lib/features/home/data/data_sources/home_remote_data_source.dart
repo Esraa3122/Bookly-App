b82abe4f6431bbc79f74dc4f526e807212a5527e
Esraa El-sheikh <esraa3122@outlook.com>
2026-04-13 17:45:39 +0200

    implement fetch newest featured books local data source

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -28,6 +28,7 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
       endPoint: 'volumes?filter=free-ebooks&Sorting=newest&q=programming',
     );
     List<BookEntity> books = getBooksList(data);
+    saveBooksData(books, kNewestBox);
     return books;
   }
 
