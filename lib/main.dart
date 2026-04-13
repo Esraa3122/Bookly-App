a9daa94dd276428675dd9bc31f2c9edf49b63b98
Esraa El-sheikh <esraa3122@outlook.com>
2026-04-13 17:29:11 +0200

    regester adapter and open featurd box

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
 // import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
 
-void main() {
+void main() async{
   runApp(const BooklyApp());
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(kFeaturedBox);
 }
 
 class BooklyApp extends StatelessWidget {
@@ -22,7 +26,7 @@ class BooklyApp extends StatelessWidget {
       ),
     );
 
-    // -- Get -- طريقة استخدام 
+    // -- Get -- طريقة استخدام
 
     // GetMaterialApp(
     //   debugShowCheckedModeBanner: false,
