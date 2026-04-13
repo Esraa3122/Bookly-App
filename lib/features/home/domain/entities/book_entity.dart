eb1d5ff7191c4563d03859990cdfa2281ee1d472
Esraa El-sheikh <esraa3122@outlook.com>
2026-04-13 04:32:33 +0200

    home_repo

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,15 @@
+class BookEntity {
+  BookEntity(
+    this.image,
+    this.title,
+    this.authorName,
+    this.price,
+    this.rating,
+  );
+  final String image;
+  final String title;
+  final String authorName;
+  final num price;
+  final num rating;
+
+}
