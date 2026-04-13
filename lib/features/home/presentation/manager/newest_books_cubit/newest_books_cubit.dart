111f603c59137236812c02fb54f2f8a006b7b729
Esraa El-sheikh <esraa3122@outlook.com>
2026-04-14 01:23:51 +0200

    getit & simple bloc observer

--- lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -1,18 +1,18 @@
-import 'package:bloc/bloc.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
-import 'package:bookly/features/home/domain/use_cases/fetch_featured_books_use_case.dart';
+import 'package:bookly/features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit(this.featuredBooksUseCase) : super(NewestBooksInitial());
+  NewestBooksCubit(this.fetchNewestBooksUseCase) : super(NewestBooksInitial());
 
-  final FetchFeaturedBooksUseCase featuredBooksUseCase;
+  final FetchNewestBooksUseCase fetchNewestBooksUseCase;
 
   Future<void> fetchNewestBooks() async {
     emit(NewestBooksLoading());
-    var result = await featuredBooksUseCase.call();
+    var result = await fetchNewestBooksUseCase.call();
 
     result.fold(
       (failure) {
